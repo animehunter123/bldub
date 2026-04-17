ac8131f072ddd788dfdde8652addd2ec4a2732c2
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2026-04-17 10:50:47 +0900

    Added new echo command for tarbackups

--- bldub.sh
+++ bldub.sh
@@ -453,42 +453,49 @@ EOF
 
 
 
+#!/usr/bin/env bash
 
-# 20250818 @@@@@@@@@@@@@@@@@@@@@@@@
-# 20250818 @@@@@@@@@@@@@@@@@@@@@@@@
-# 20250818 @@@@@@@@@@@@@@@@@@@@@@@@
-# NOW LETS GET NVIM WITH LAZYVIM AND RUST IN HERE!!!!!!
-echo -e "\e[32mInstalling pre-reqs...\e[0m"
+# Note: cc is required, this is because...I learned that telescope 
+# compiles ".so" files into ~/.local/share/nvim/site/parser
+# Thus, in order to have nvim+treesitte YOU MUST INSTALL RUST and
+# have the .cargo/.rustup dirs in theory for rust-analyzer.
+
+# @@ Install pre-requiresite apps from internet repos
+echo -e "\e[35mInstalling FROM INTERNET pre-reqs from REPOs (apt/dnf)...\e[0m"
+echo -e "\e[35mInstalling FROM INTERNET pre-reqs from REPOs (apt/dnf)...\e[0m"
+echo -e "\e[35mInstalling FROM INTERNET pre-reqs from REPOs (apt/dnf)...\e[0m"
 sudo apt-get update
-sudo apt-get install -y build-essential fzf fd-find bat ripgrep zoxide git curl unzip jq
-sudo apt install -y luajit libluajit-5.1-dev # THIS IS FOR MY CargoRun plugin, THERE MIGHT BE A NEWER VERSION IN THE FUTURE
-# For Rocky:
 sudo dnf -y update
 sudo dnf -y install epel-release
 sudo dnf -y update
 sudo dnf -y groupinstall "Development Tools"
-sudo dnf -y install \
-  fzf \
-  fd-find \
-  bat \
-  ripgrep \
-  zoxide \
-  git \
-  curl \
-  unzip \
-  jq \
-  luajit \
-  luajit-devel
+packages=(
+        build-essential fzf fd-find bat ripgrep zoxide git curl unzip jq
+        luajit libluajit-5.1-dev fd-find luajit-devel
+)
+for package in "${packages[@]}"; do
+        echo "Installing $package..."
+		apt install -y $package
+		dnf install -y $package
+done
 
+# @@ Install fzf isnt in the repo (i.e. rocky96)...
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# @@ Install Rustup
 echo "INSTALLING RUSTUP!!!!!!!!!!"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"            # For sh/bash/zsh/ash/dash/pdksh
 source "$HOME/.cargo/env.fish"  # For fish
 rustup component add rust-analyzer rustfmt clippy rust-src
+
 # Optional, I disabled to speed up this installer
 # cargo install rust-analyzer
 # cargo install cargo-edit
 # cargo install cargo-watch
+
 echo -e "\e[32mInstalling Neovim (LazyVim base)...\e[0m"
 timestamp=$(date +"%Y%m%d%H%M%S")
 rm -f nvim.appimage
@@ -502,6 +509,7 @@ sudo cp nvim-linux-x86_64.appimage /bin/ 2>/dev/null
 sudo mv /bin/nvim-linux-x86_64.appimage /bin/nvim
 sudo chmod 777 /bin/nvim
 sudo ln -s /bin/nvim /bin/n
+
 echo -e "\e[32mClearing old LazyVim config...\e[0m"
 if [ -d "$HOME/.config/nvim" ]; then
   mv "$HOME/.config/nvim" "$HOME/.config/nvim.$timestamp"
@@ -509,11 +517,14 @@ fi
 if [ -d "$HOME/.local/share/nvim" ]; then
   mv "$HOME/.local/share/nvim" "$HOME/.local/share/nvim.$timestamp"
 fi
+
 echo -e "\e[32mInstalling LazyVim starter config...\e[0m"
 git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
 rm -rf "$HOME/.config/nvim/.git"
+
 echo RUNNING MASON INSTALLER PER THEIR GUIDE...
 echo "
+
 -- This is so that we can get MasonInstall instead of just Mason
 require(\"mason\").setup()
 " >> "$HOME/.config/nvim/init.lua"
@@ -566,6 +577,9 @@ pushd .
 cd ~/.local/share/nvim/lazy/cargo.nvim
 cargo build --release
 popd
+
+
+
 echo "INSTALL MY NORMAL PLUGINS I WANTED VIA MASONINSTALL CLI..."
 nvim --headless -c "MasonInstall rust-analyzer" -c "qall"
 nvim --headless -c "MasonInstall rustfmt" -c "qall"
@@ -573,6 +587,7 @@ nvim --headless -c "MasonInstall codelldb" -c "qall"
 # nvim --headless -c "MasonInstall cpptools" -c "qall"
 nvim --headless -c "MasonInstall crates.nvim" -c "qall"
 nvim --headless -c "MasonInstall rustaceanvim" -c "qall"
+
 echo "INSTALL MY EXTRA PLUGINS I WANTED VIA MASONINSTALL CLI... (launch nvim once, sed the setup() into the plugins, and the next time you launch AFTER this script it installs rust extras)"
 awk '
 /{[ ]*import[ ]*=[ ]*"plugins"[ ]*}/ {
@@ -581,6 +596,7 @@ awk '
 }
 { print }
 ' ~/.config/nvim/lua/config/lazy.lua > /tmp/lazy.lua && mv /tmp/lazy.lua ~/.config/nvim/lua/config/lazy.lua
+
 pushd .
 cd /tmp
 cargo new nvim-test01
@@ -589,9 +605,11 @@ nvim src/main.rs # dummy filename to ensure any downloads are needed?
 cd ..
 rm -rf nvim-test01
 popd
+
 jq '.extras += ["lazyvim.plugins.extras.lang.rust", "lazyvim.plugins.extras.editor.telescope"]' \
   ~/.config/nvim/lazyvim.json > /tmp/lazyvim.json \
   && mv /tmp/lazyvim.json ~/.config/nvim/lazyvim.json
+
 echo 'OK INSTALL COMPLETE
 To Debug Rust, Be sure to install or confirm: 
 (Normal Plugins) i >>
@@ -602,8 +620,10 @@ lang.rust  <-- for rust basic
 test.core  <-- for debug
 OK PRESS ANY BUTTON TO CONTINUE!!
 '
+
 # Autosave : This is my Auto-save Automatically every 1 second!!! I PUT THIS IN DISABLED B/C IT AUTOFORMATS ON SAVE WHICH IS ANNOYING
 cat >> "$HOME/.config/nvim/init.lua_DISABLEDTHIS" << 'EOF'
+
 -- Auto-save all modified buffers every 1 second
 local timer = vim.loop.new_timer()
 timer:start(
@@ -623,7 +643,22 @@ timer:start(
   end)
 )
 EOF
+
+echo "...
+
+Cool!
+
+You should now have a appimage and 3 folders ./local/state/nvim ./local/share/nvim ./config/nvim
+
+To back this up as your MYUSERNAME, use: 
+cd ~
+tar cvfpz lazyvim_v0.12.1_MYUSERNAME_with_rust.tar.gz ./nvim-linux-x86_64.appimage ./.config/nvim/ ./.local/share/nvim ./.local/state/nvim ./.cache/nvim/ ./.fzf ./.cargo/ ./.rustup/
+
+"
+
 echo "Script complete! No need to relaunch nvim!"
+
+
 
 
 
