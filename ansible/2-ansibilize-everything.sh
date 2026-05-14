277d1a22a40384f452138367a894d11088cc3959
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2026-05-15 07:37:53 +0900

    Added a playbook to install ubuntu

--- ansible/2-ansibilize-everything.sh
+++ ansible/2-ansibilize-everything.sh
@@ -5,5 +5,9 @@ echo "
 ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-openup.yml
 
 echo "
+@@ Installing docker-ce UBUNTU"
+ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-install-docker #--ask-become-pass
+
+echo "
 @@ SETTING P@ pass"
 ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-set-root-password.yml #--ask-become-pass
