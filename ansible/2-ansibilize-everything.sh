05acebc8ca73f3dc450c648f210df03703a67726
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2026-05-18 09:21:55 +0900

    Added cifs-utils and nfs-utils

--- ansible/2-ansibilize-everything.sh
+++ ansible/2-ansibilize-everything.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 echo "
-@@ OPENING UP UBUNTU"
+@@ OPENING UP UBUNTU..."
 ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-openup.yml
 
 echo "
-@@ Installing docker-ce UBUNTU"
+@@ Installing docker-ce UBUNTU..."
 ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-install-docker #--ask-become-pass
 
 echo "
-@@ SETTING P@ pass"
-ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-set-root-password.yml #--ask-become-pass
+@@ SETTING P@ pass..."
+ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-set-root-password.yml 
+
+echo "
+@@ INSTALLING cifs-utils and nfs-utils..."
+ansible-playbook -i inventory/hosts.ini playbooks/ubuntu-install-nfs_cifs.yml
